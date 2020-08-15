a5c7170fcfac5f7c2fac8a2193c5d3fa087815dd
woodspring.toronto <woodspring.toronto@gmail.com>
2020-08-15 13:51:12 -0400

    add firstsrc/test/java/woodspring/springmills/SpringMillsApplicationTests.java

---
+++ src/test/java/woodspring/springmills/SpringMillsApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springmills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
